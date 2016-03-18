9a3a5c733d6cf1f7d3a1b16873dde08d1b0b30bc
alan <1561713952@qq.com>
2016-03-18 19:25:38 +0800

    Merge branch 'master' of https://github.com/i-HaQi/TestMaven
    # Conflicts:
    #	src/test/java/com/alan/demo/AppTest.java

--- src/main/java/com/alan/demo/App.java
+++ src/main/java/com/alan/demo/App.java
@@ -5,6 +5,6 @@ package com.alan.demo;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("kello World!");
+        System.out.println("eello World!");
     }
 }
