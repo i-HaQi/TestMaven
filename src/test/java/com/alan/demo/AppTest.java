34b00dc6f3359434b4c28275d21569c7912b93d1
alan <1561713952@qq.com>
2016-03-18 17:40:46 +0800

    TEST2

--- src/test/java/com/alan/demo/AppTest.java
+++ src/test/java/com/alan/demo/AppTest.java
@@ -10,7 +10,7 @@ import junit.framework.TestSuite;
 public class AppTest
         extends TestCase {
     /**
-     *
+     *nihkekkkfjd
      * @param testName name of the test case
      */
     public AppTest(String testName) {
